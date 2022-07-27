b20db08641657ec53977b89509eecc97fc018a60
Chris72 <93304927+javito72@users.noreply.github.com>
2022-07-27 19:53:44 -0300

    add preauthorize
    se agrego preauthorize en netbeans

--- BackEnd/src/main/java/com/portfolio/cjl/Controller/CEducacion.java
+++ BackEnd/src/main/java/com/portfolio/cjl/Controller/CEducacion.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,6 +42,7 @@ public class CEducacion {
         return new ResponseEntity(educacion, HttpStatus.OK);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id) {
         if (!sEducacion.existsById(id)) {
@@ -50,7 +52,7 @@ public class CEducacion {
         return new ResponseEntity(new Mensaje("Educación eliminada"), HttpStatus.OK);
     }
 
-    
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody dtoEducacion dtoeducac){      
         if(StringUtils.isBlank(dtoeducac.getNombreEd()))
@@ -64,6 +66,7 @@ public class CEducacion {
         return new ResponseEntity(new Mensaje("Educación agregada"), HttpStatus.OK);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoEducacion dtoeducac){
         //Validamos si existe el ID
--- BackEnd/src/main/java/com/portfolio/cjl/Controller/CProyecto.java
+++ BackEnd/src/main/java/com/portfolio/cjl/Controller/CProyecto.java
@@ -12,6 +12,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,6 +44,7 @@ public class CProyecto {
         return new ResponseEntity(proyecto, HttpStatus.OK);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id) {
         if (!sProyecto.existsById(id)) {
@@ -52,7 +54,7 @@ public class CProyecto {
         return new ResponseEntity(new Mensaje("Proyecto eliminado"), HttpStatus.OK);
     }
 
-    
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody dtoProyecto dtoproye){      
         if(StringUtils.isBlank(dtoproye.getNombreP()))
@@ -66,6 +68,7 @@ public class CProyecto {
         return new ResponseEntity(new Mensaje("Proyecto agregado"), HttpStatus.OK);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoProyecto dtoproye){
         //Validamos si existe el ID
